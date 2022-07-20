e3102add0dd9d699069fea1bccd190b1eb846172
Vadim <vadicrugun@gmail.com>
2022-07-20 15:33:14 +0300

    Executed practice at lesson 24 part 2 of a JS Basic chapter

--- script.js
+++ script.js
@@ -1,27 +1,27 @@
 /* Задание на урок:
 
-1) Создать переменную numberOfFilms и в неё поместить ответ от пользователя на вопрос:
-'Сколько фильмов вы уже посмотрели?'
+1) Автоматизировать вопросы пользователю про фильмы при помощи цикла
 
-2) Создать объект personalMovieDB и в него поместить такие свойства:
-    - count - сюда передается ответ на первый вопрос
-    - movies - в это свойство поместить пустой объект
-    - actors - тоже поместить пустой объект
-    - genres - сюда поместить пустой массив
-    - privat - в это свойство поместить boolean(логическое) значение false
+2) Сделать так, чтобы пользователь не мог оставить ответ в виде пустой строки,
+отменить ответ или ввести название фильма длинее, чем 50 символов. Если это происходит - 
+возвращаем пользователя к вопросам опять
 
-3) Задайте пользователю по два раза вопросы:
-    - 'Один из последних просмотренных фильмов?'
-    - 'На сколько оцените его?'
-Ответы стоит поместить в отдельные переменные
-Записать ответы в объект movies в формате: 
-    movies: {
-        'logan': '8.1'
-    }
+3) При помощи условий проверить  personalMovieDB.count, и если он меньше 10 - вывести сообщение
+"Просмотрено довольно мало фильмов", если от 10 до 30 - "Вы классический зритель", а если больше - 
+"Вы киноман". А если не подошло ни к одному варианту - "Произошла ошибка"
 
-Проверить, чтобы все работало без ошибок в консоли */
+4) Потренироваться и переписать цикл еще двумя способами*/
 
-const numberOfFilms = prompt("How many movies have you seen?");
+'use strict';
+
+// Код возьмите из предыдущего домашнего задания
+
+let numberOfFilms = null;
+
+do {
+    numberOfFilms = prompt("How many movies have you seen?");
+    console.log(numberOfFilms);
+} while (numberOfFilms === null || numberOfFilms === '' || numberOfFilms.length > 50);
 
 const personalMovieDB = {
     count: numberOfFilms,
@@ -37,8 +37,58 @@ const questions = {
 };
 
 for (let i = 0; i < 2; i++) {
-    const a = prompt(questions.lastWatchedMovie, "");
-    const b = prompt(questions.rate, "");
+    const a = prompt(questions.lastWatchedMovie + " " + i, "");
+    const b = prompt(questions.rate + " " + i, "");
 
+    
+    if (
+        a === null || 
+        a === '' || 
+        a.length > 50 || 
+        b === null || 
+        b === '' || 
+        b.length > 50
+    ) {
+        i--;
+        continue;
+    }
+        
     personalMovieDB.movies[a] = b;
+
 }
+
+/* let i = 0;
+
+do {
+    const a = prompt(questions.lastWatchedMovie + " " + i, "");
+    const b = prompt(questions.rate + " " + i, "");
+    
+    if (
+        a === null || 
+        a === '' || 
+        a.length > 50 || 
+        b === null || 
+        b === '' || 
+        b.length > 50
+    ) {
+        i = (i === 0) ? 0 : i - 1;
+        continue;
+    }
+        
+    personalMovieDB.movies[a] = b;
+
+    i++;
+} while (i < 2); */
+
+
+if (personalMovieDB.count < 10) {
+    alert("You have watched only few movies");
+} else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
+    alert("You are classic viewer");
+} else if (personalMovieDB.count >= 30) {
+    alert("You are cinephile");
+} else {
+    alert("Ooops, we have got an error...");
+}
+
+console.log(personalMovieDB.count >= 30);
